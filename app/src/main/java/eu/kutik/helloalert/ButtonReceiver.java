87daafcfd8093d466cefa69e09561d1d3d096456
Arun <v.arunindia@gmail.com>
2020-06-29 16:22:21 +0530

    show received intent on screen

--- app/src/main/java/eu/kutik/helloalert/ButtonReceiver.java
+++ app/src/main/java/eu/kutik/helloalert/ButtonReceiver.java
@@ -19,6 +19,7 @@ public class ButtonReceiver extends BroadcastReceiver {
       Intent act = new Intent(context, MainActivity.class);
       act.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
       act.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+      act.putExtra(MainActivity.INTENT_STRING, intent.getAction());
       context.startActivity(act);
     }
   }
--- app/src/main/java/eu/kutik/helloalert/MainActivity.java
+++ app/src/main/java/eu/kutik/helloalert/MainActivity.java
@@ -3,8 +3,12 @@ package eu.kutik.helloalert;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+  public static final String INTENT_STRING = "intent_string";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +17,10 @@ public class MainActivity extends Activity {
         WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
         WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
         WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-
     setContentView(R.layout.activity_main);
+    TextView text = (TextView) findViewById(R.id.textBox);
+    if ( text !=null && getIntent().getStringExtra(INTENT_STRING) != null ) {
+        text.setText("Received intent: " + getIntent().getStringExtra(INTENT_STRING));
+    }
   }
 }
