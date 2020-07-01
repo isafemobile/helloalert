0e0098b1db016196ed64fa4b1920a8b4dc4d5f73
Daniel Kutik <daniel.kutik@lavawerk.com>
2020-07-01 19:11:03 +0800

    Introduced internal intent
    We don't want the activity to be restarted
    constantly.

--- app/src/main/java/eu/kutik/helloalert/ButtonReceiver.java
+++ app/src/main/java/eu/kutik/helloalert/ButtonReceiver.java
@@ -1,20 +1,42 @@
 package eu.kutik.helloalert;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.view.KeyEvent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import java.util.List;
 
 public class ButtonReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, final Intent intent) {
         if (context != null && intent != null) {
-            Intent act = new Intent(context, MainActivity.class);
-            act.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            act.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            act.putExtra(MainActivity.INTENT_STRING, intent.getAction());
-            context.startActivity(act);
+            final PackageManager pm = context.getPackageManager();
+            if (pm != null) {
+                final Intent event = new Intent(MainActivity.ACTION_NEW_EVENT);
+                event.putExtra(MainActivity.EVENT_EXTRA, intent.getAction());
+
+                final List<ResolveInfo> receivers = pm.queryBroadcastReceivers(event, 0);
+                if (receivers.size() > 0) {
+                    for (ResolveInfo r : receivers) {
+                        final Intent explicit = new Intent(event);
+                        final ComponentName cn = new ComponentName(r.activityInfo.applicationInfo.packageName,
+                                r.activityInfo.name);
+                        explicit.setComponent(cn);
+                        context.sendBroadcast(explicit);
+                    }
+                } else {
+                    // Activity not started yet...
+                    final Intent activity = new Intent(context, MainActivity.class);
+                    activity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    activity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    activity.putExtra(MainActivity.EVENT_EXTRA, intent.getAction());
+                    context.startActivity(activity);
+                }
+            }
         }
     }
 }
--- app/src/main/java/eu/kutik/helloalert/MainActivity.java
+++ app/src/main/java/eu/kutik/helloalert/MainActivity.java
@@ -2,28 +2,63 @@ package eu.kutik.helloalert;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.WindowManager;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    public static final String INTENT_STRING = "intent_string";
-    private static int count = 1;
+    public static final String ACTION_NEW_EVENT = "eu.kutik.helloalert.ACTION_NEW_EVENT";
+    public static final String EVENT_EXTRA = "extra_intent";
 
-    @SuppressLint("SetTextI18n")
+    private final IntentFilter filter = new IntentFilter(ACTION_NEW_EVENT);
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+            if (intent != null && intent.hasExtra(EVENT_EXTRA)) {
+                onNewEvent(intent.getStringExtra(EVENT_EXTRA));
+            }
+        }
+    };
+
+    private int count = 1;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onResume() {
+        super.onResume();
+        this.registerReceiver(receiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
                 WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
                 WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
                 WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         setContentView(R.layout.activity_main);
+
+        final Intent intent = getIntent();
+        if (intent != null && intent.hasExtra(EVENT_EXTRA)) {
+            onNewEvent(intent.getStringExtra(EVENT_EXTRA));
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void onNewEvent(final String action) {
         TextView text = (TextView) findViewById(R.id.textBox);
-        if (text != null && getIntent().getStringExtra(INTENT_STRING) != null) {
-            text.setText("Received intent: (" + count++ + ") :" + getIntent().getStringExtra(INTENT_STRING));
+        if (text != null) {
+            text.setText("Received intent: (" + count++ + ") :" + action);
         }
     }
 }
