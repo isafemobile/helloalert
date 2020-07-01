edbeb72e1d95ad236b94a1edd193fdf3353a8f38
Arun <v.arunindia@gmail.com>
2020-07-01 15:41:57 +0530

    Autoformatted code

--- app/src/main/java/eu/kutik/helloalert/ButtonReceiver.java
+++ app/src/main/java/eu/kutik/helloalert/ButtonReceiver.java
@@ -7,14 +7,14 @@ import android.view.KeyEvent;
 
 public class ButtonReceiver extends BroadcastReceiver {
 
-  @Override
-  public void onReceive(final Context context, final Intent intent) {
-    if (context != null && intent != null) {
-      Intent act = new Intent(context, MainActivity.class);
-      act.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      act.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-      act.putExtra(MainActivity.INTENT_STRING, intent.getAction());
-      context.startActivity(act);
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        if (context != null && intent != null) {
+            Intent act = new Intent(context, MainActivity.class);
+            act.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            act.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            act.putExtra(MainActivity.INTENT_STRING, intent.getAction());
+            context.startActivity(act);
+        }
     }
-  }
 }
--- app/src/main/java/eu/kutik/helloalert/MainActivity.java
+++ app/src/main/java/eu/kutik/helloalert/MainActivity.java
@@ -9,20 +9,21 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-  public static final String INTENT_STRING = "intent_string";
-  private static int count = 1;
-  @SuppressLint("SetTextI18n")
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
-        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
-        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-    setContentView(R.layout.activity_main);
-    TextView text = (TextView) findViewById(R.id.textBox);
-    if ( text !=null && getIntent().getStringExtra(INTENT_STRING) != null ) {
-        text.setText("Received intent: (" + count++ + ") :" + getIntent().getStringExtra(INTENT_STRING));
+    public static final String INTENT_STRING = "intent_string";
+    private static int count = 1;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+        TextView text = (TextView) findViewById(R.id.textBox);
+        if (text != null && getIntent().getStringExtra(INTENT_STRING) != null) {
+            text.setText("Received intent: (" + count++ + ") :" + getIntent().getStringExtra(INTENT_STRING));
+        }
     }
-  }
 }
