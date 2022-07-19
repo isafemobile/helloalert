e2dde929f0dfb91f47eaf65d7461b30dd083cb33
Dominik Baronelli <dominik.baronelli@lavawerk.com>
2022-07-19 11:44:22 +0200

    Added timestamps to output
    Signed-off-by: Daniel Kutik <daniel.kutik@lavawerk.com>

--- app/src/main/java/com/lavawerk/test/helloalert/ButtonReceiver.java
+++ app/src/main/java/com/lavawerk/test/helloalert/ButtonReceiver.java
@@ -3,29 +3,28 @@ package com.lavawerk.test.helloalert;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.os.SystemClock;
 
 public class ButtonReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, final Intent intent) {
         if (context != null && intent != null) {
-            final PackageManager pm = context.getPackageManager();
-            if (pm != null) {
+            long receiverTime = SystemClock.uptimeMillis();
+            if (MainActivity.active) {
                 final Intent event = new Intent(MainActivity.ACTION_NEW_EVENT);
-                event.putExtra(MainActivity.EVENT_EXTRA, intent.getAction());
-
-                if (MainActivity.active) {
-                        final Intent implicit = new Intent(event);
-                        context.sendBroadcast(implicit);
-                } else {
-                    // Activity not started yet...
-                    final Intent activity = new Intent(context, MainActivity.class);
-                    activity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    activity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    activity.putExtra(MainActivity.EVENT_EXTRA, intent.getAction());
-                    context.startActivity(activity);
-                }
+                event.putExtra(MainActivity.EXTRA_INTENT, intent);
+                event.putExtra(MainActivity.EXTRA_RECEIVER_TIME, receiverTime);
+                final Intent implicit = new Intent(event);
+                context.sendBroadcast(implicit);
+            } else {
+                // Activity not started yet...
+                final Intent activity = new Intent(context, MainActivity.class);
+                activity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                activity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                activity.putExtra(MainActivity.EXTRA_INTENT, intent);
+                activity.putExtra(MainActivity.EXTRA_RECEIVER_TIME, receiverTime);
+                context.startActivity(activity);
             }
         }
     }
--- app/src/main/java/com/lavawerk/test/helloalert/MainActivity.java
+++ app/src/main/java/com/lavawerk/test/helloalert/MainActivity.java
@@ -7,26 +7,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.KeyEvent;
 import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     public static final String ACTION_NEW_EVENT = "com.lavawerk.test.helloalert.ACTION_NEW_EVENT";
-    public static final String EVENT_EXTRA = "extra_intent";
-
+    public static final String EXTRA_INTENT = "extra_intent";
+    public static final String EXTRA_RECEIVER_TIME = "extra_receiver_time";
+    public static boolean active = false;
+    private static int count = 1;
     private final IntentFilter filter = new IntentFilter(ACTION_NEW_EVENT);
     private final BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(final Context context, final Intent intent) {
-            if (intent != null && intent.hasExtra(EVENT_EXTRA)) {
-                onNewEvent(intent.getStringExtra(EVENT_EXTRA));
-            }
+            onNewEvent(intent);
         }
     };
-
-    private static int count = 1;
-    public static boolean  active = false;
 
     @Override
     protected void onResume() {
@@ -51,16 +50,36 @@ public class MainActivity extends Activity {
                 WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         setContentView(R.layout.activity_main);
         final Intent intent = getIntent();
-        if (intent != null && intent.hasExtra(EVENT_EXTRA)) {
-            onNewEvent(intent.getStringExtra(EVENT_EXTRA));
-        }
+        onNewEvent(intent);
     }
 
     @SuppressLint("SetTextI18n")
-    public void onNewEvent(final String action) {
+    public void onNewEvent(final Intent intent) {
         TextView text = (TextView) findViewById(R.id.textBox);
         if (text != null) {
-            text.setText("Received intent: (" + count++ + ") :" + action);
+            if (intent != null && intent.hasExtra(EXTRA_INTENT)) {
+                Intent buttonIntent = intent.getParcelableExtra(EXTRA_INTENT);
+
+                long receiverTime = intent.getLongExtra(EXTRA_RECEIVER_TIME, 0);
+                String action = buttonIntent.getAction();
+
+                long eventTime = 0;
+                KeyEvent keyEvent = buttonIntent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+                if (keyEvent != null) {
+                    eventTime = keyEvent.getEventTime();
+                }
+
+                text.append("[" + count + "] " + action + "\n");
+                if (receiverTime > 0) {
+                    long deltaReceiverTime = SystemClock.uptimeMillis() - receiverTime;
+                    text.append("[" + count + "] deltaReceiverTime: " + deltaReceiverTime + "ms\n");
+                }
+                if (eventTime > 0) {
+                    long deltaEventTime = SystemClock.uptimeMillis() - eventTime;
+                    text.append("[" + count + "] deltaEventTime: " + deltaEventTime + "ms\n");
+                }
+                count++;
+            }
         }
     }
 }
